066b9b677b64149e44fcdc20ee5d6e506404d807
Dmytro Slipchenko <dslipchenko@gmail.com>
2023-08-15 23:51:06 +0300

    removed: cartItems, Button component

--- src/components/index.js
+++ src/components/index.js
@@ -1,5 +1,4 @@
 export { default as AppRouter } from './AppRouter';
-export { default as Button } from './Button';
 export { default as Card } from './Card';
 export { default as Catalog } from './Catalog';
 export { default as Header } from './Header';
--- src/context/BooksContext.js
+++ src/context/BooksContext.js
@@ -5,14 +5,11 @@ const BooksContext = createContext();
 
 const BooksContextProvider = ({ children }) => {
 	const [booksData, setBooksData] = useState([]);
-	const [cartItems, setCartItems] = useState([]);
 	const [state, dispatch] = useReducer(cartReducer, { cart: [] });
 
 	const value = {
 		booksData,
 		setBooksData,
-		cartItems,
-		setCartItems,
 		state,
 		dispatch,
 	};
